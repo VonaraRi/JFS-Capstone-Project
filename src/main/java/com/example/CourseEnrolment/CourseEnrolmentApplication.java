cf7bcbf29f76898129dfb53dbf75abedfcf982b3
Rionnalyn <rionnalynrichard5@gmail.com>
2026-08-12 14:59:13 +0800

    Add files via upload

---
+++ src/main/java/com/example/CourseEnrolment/CourseEnrolmentApplication.java
@@ -0,0 +1,13 @@
+package com.example.CourseEnrolment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseEnrolmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseEnrolmentApplication.class, args);
+	}
+
+}
